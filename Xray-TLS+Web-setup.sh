82d1f733d1ddc43122d566bf85b6e75465d15745
kirin10000 <57820613+kirin10000@users.noreply.github.com>
2021-02-11 00:55:27 +0800

    fix

--- Xray-TLS+Web-setup.sh
+++ Xray-TLS+Web-setup.sh
@@ -1608,12 +1608,9 @@ cat > ${nginx_prefix}/conf.d/nextcloud.conf <<EOF
         access_log off;
     }
     location ^~ /.well-known {
-        rewrite ^/\\.well-known/host-meta\\.json  /public.php?service=host-meta-json  last;
-        rewrite ^/\\.well-known/host-meta        /public.php?service=host-meta       last;
-        rewrite ^/\\.well-known/webfinger        /public.php?service=webfinger       last;
-        rewrite ^/\\.well-known/nodeinfo         /public.php?service=nodeinfo        last;
         location = /.well-known/carddav     { return 301 https://\$host/remote.php/dav/; }
         location = /.well-known/caldav      { return 301 https://\$host/remote.php/dav/; }
+        location ^~ /.well-known            { return 301 https://\$host/index.php\$uri; }
         try_files \$uri \$uri/ =404;
     }
     location ~ ^/(?:build|tests|config|lib|3rdparty|templates|data)(?:$|/)  { return 404; }
