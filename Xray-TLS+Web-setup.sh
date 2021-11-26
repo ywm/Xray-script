8e3da4dffa7ba33c2ce47b46f9aedb152f23f54f
kirin10000 <57820613+kirin10000@users.noreply.github.com>
2021-11-26 21:30:38 +0800

    update cloudreve && php && nextcloud

--- Xray-TLS+Web-setup.sh
+++ Xray-TLS+Web-setup.sh
@@ -25,17 +25,17 @@ nginx_config="${nginx_prefix}/conf.d/xray.conf"
 nginx_service="/etc/systemd/system/nginx.service"
 nginx_is_installed=""
 
-php_version="php-8.0.12"
+php_version="php-8.1.0"
 php_prefix="/usr/local/php"
 php_service="/etc/systemd/system/php-fpm.service"
 php_is_installed=""
 
-cloudreve_version="3.3.2"
+cloudreve_version="3.4.1"
 cloudreve_prefix="/usr/local/cloudreve"
 cloudreve_service="/etc/systemd/system/cloudreve.service"
 cloudreve_is_installed=""
 
-nextcloud_url="https://download.nextcloud.com/server/releases/nextcloud-22.2.0.zip"
+nextcloud_url="https://download.nextcloud.com/server/releases/nextcloud-22.2.3.zip"
 
 xray_config="/usr/local/etc/xray/config.json"
 xray_is_installed=""
