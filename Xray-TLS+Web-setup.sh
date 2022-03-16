e48379f1fc1dc3b6491df7db9b1acde8f34bf7f2
kirin10000 <57820613+kirin10000@users.noreply.github.com>
2022-03-16 13:44:53 +0800

    update openssl

--- Xray-TLS+Web-setup.sh
+++ Xray-TLS+Web-setup.sh
@@ -19,7 +19,7 @@ timezone=""
 
 #安装信息
 nginx_version="nginx-1.21.6"
-openssl_version="openssl-openssl-3.0.1"
+openssl_version="openssl-openssl-3.0.2"
 nginx_prefix="/usr/local/nginx"
 nginx_config="${nginx_prefix}/conf.d/xray.conf"
 nginx_service="/etc/systemd/system/nginx.service"
