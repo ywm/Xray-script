ca1e7f2b0d60eff11c3d36a2a5809b84187733a5
kirin10000 <57820613+kirin10000@users.noreply.github.com>
2022-05-05 23:21:58 +0800

    Update openssl  && nextcloud

--- Xray-TLS+Web-setup.sh
+++ Xray-TLS+Web-setup.sh
@@ -19,7 +19,7 @@ timezone=""
 
 #安装信息
 nginx_version="nginx-1.21.6"
-openssl_version="openssl-openssl-3.0.2"
+openssl_version="openssl-openssl-3.0.3"
 nginx_prefix="/usr/local/nginx"
 nginx_config="${nginx_prefix}/conf.d/xray.conf"
 nginx_service="/etc/systemd/system/nginx.service"
@@ -35,7 +35,7 @@ cloudreve_prefix="/usr/local/cloudreve"
 cloudreve_service="/etc/systemd/system/cloudreve.service"
 cloudreve_is_installed=""
 
-nextcloud_url="https://download.nextcloud.com/server/prereleases/nextcloud-24.0.0rc3.zip"
+nextcloud_url="https://download.nextcloud.com/server/releases/nextcloud-24.0.0.zip"
 
 xray_config="/usr/local/etc/xray/config.json"
 xray_is_installed=""
