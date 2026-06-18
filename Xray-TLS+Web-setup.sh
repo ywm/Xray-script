5d4d7d216a03d8b21d786a7c48c7c6dc07a538ef
ywm <ywm4125@gmail.com>
2026-06-18 01:23:01 -0700

    update nginx openssl

--- Xray-TLS+Web-setup.sh
+++ Xray-TLS+Web-setup.sh
@@ -42,8 +42,8 @@ unset timezone
 unset ssh_service
 
 #安装配置信息
-nginx_version="nginx-1.31.1"
-openssl_version="openssl-3.6.2"
+nginx_version="nginx-1.31.2"
+openssl_version="openssl-3.6.3"
 nginx_prefix="/usr/local/nginx"
 nginx_config="${nginx_prefix}/conf/nginx.conf"
 nginx_service="/etc/systemd/system/nginx.service"
